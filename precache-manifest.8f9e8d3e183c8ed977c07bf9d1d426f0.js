97e8232f9c9dabb0ad9978001362abe0bb9999d1
Mhamad Sarkawt <hamasarkawt1999@gmail.com>
2021-02-09 19:22:06 -0800

    Updates

---
+++ precache-manifest.8f9e8d3e183c8ed977c07bf9d1d426f0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361f14cb010d5b33476fd34b8cb0cad5",
+    "url": "/Zodiac/index.html"
+  },
+  {
+    "revision": "792080cb97a34fb3f7bf",
+    "url": "/Zodiac/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1ae2de4c176509221df9",
+    "url": "/Zodiac/static/css/main.26302377.chunk.css"
+  },
+  {
+    "revision": "792080cb97a34fb3f7bf",
+    "url": "/Zodiac/static/js/2.41038122.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zodiac/static/js/2.41038122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae2de4c176509221df9",
+    "url": "/Zodiac/static/js/main.ef4cd47a.chunk.js"
+  },
+  {
+    "revision": "f068fd41ba0d180f29f0",
+    "url": "/Zodiac/static/js/runtime-main.658e7118.js"
+  },
+  {
+    "revision": "a0756104f72d7757ec4d48e6e08e81d2",
+    "url": "/Zodiac/static/media/aquarius.a0756104.png"
+  },
+  {
+    "revision": "0e63155c3646f70966e1d0a85042a3f6",
+    "url": "/Zodiac/static/media/aries.0e63155c.png"
+  },
+  {
+    "revision": "ebaa3be84058e04e53d72ee4fa7388f7",
+    "url": "/Zodiac/static/media/bg.ebaa3be8.jpg"
+  },
+  {
+    "revision": "3ae4c2c0296e5558b397ac254a77e7b8",
+    "url": "/Zodiac/static/media/cancer.3ae4c2c0.png"
+  },
+  {
+    "revision": "cf50972a076987a219574edc6121adc6",
+    "url": "/Zodiac/static/media/capricorn.cf50972a.png"
+  },
+  {
+    "revision": "7fd8ff29aa5275d9f384a32e45a595c4",
+    "url": "/Zodiac/static/media/gemini.7fd8ff29.png"
+  },
+  {
+    "revision": "76fa1d47129c3780a6d170498e9a1b17",
+    "url": "/Zodiac/static/media/leo.76fa1d47.png"
+  },
+  {
+    "revision": "b1cd67c10e707296ec5795beff098afa",
+    "url": "/Zodiac/static/media/libra.b1cd67c1.png"
+  },
+  {
+    "revision": "60ab58e605ac0650a894f664f9e05449",
+    "url": "/Zodiac/static/media/logo.60ab58e6.png"
+  },
+  {
+    "revision": "1d6912d6088426ebc638894bc9287010",
+    "url": "/Zodiac/static/media/pisces.1d6912d6.png"
+  },
+  {
+    "revision": "551a0221ac4261906b6666dca9e748c4",
+    "url": "/Zodiac/static/media/sagittarius.551a0221.png"
+  },
+  {
+    "revision": "33c2ecd8436626b9d3ee35dcab68c2fb",
+    "url": "/Zodiac/static/media/scorpio.33c2ecd8.png"
+  },
+  {
+    "revision": "e7f94bfcff20581db5c7f1c37ac7427d",
+    "url": "/Zodiac/static/media/taurus.e7f94bfc.png"
+  },
+  {
+    "revision": "8ded19520de4885afb82e154eedf2c94",
+    "url": "/Zodiac/static/media/virgo.8ded1952.png"
+  },
+  {
+    "revision": "4bd2a44f35a2651093bcb9c44aa11bb8",
+    "url": "/Zodiac/static/media/zodiac.4bd2a44f.woff"
+  }
+]);
